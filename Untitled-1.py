eafbdd99e35cd2a9f977ec3ff33a31d579135c52
vatikalashashank <vatikalashashank@gmail.com>
2025-01-15 09:00:49 +0530

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+# a,*b=10,20,'c'
+# print(a)
+# print(b)
+# print(type(a))
+# print('b',type(b))
+# c=11,22,33
+# print(c)
+# print('c',type(c))
+
+# a=()
+# print(type(a))
+# b=(1)
+# print(type(b))
+# c=(1,)
+# print(type(c))
+
